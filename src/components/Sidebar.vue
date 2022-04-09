b790a333bd043a1620f08b4eac82cd3a408a99a6
TranNhan <trannhan.s3@gmail.com>
2022-04-09 17:00:31 +0700

    finish

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,63 @@
+<template>
+  <aside :class="`${is_expanded ? 'is-expanded' : ''}`">
+    <div class="logo">
+      <img :src="logoURL" alt="Vue" />
+    </div>
+
+    <div class="menu-toggle-wrap">
+      <button class="menu-toggle" @click="ToggleMenu">
+        <span class="material-icons"
+          >keyboard_double_arrow_right</span
+        >
+      </button>
+    </div>
+
+    <h3>Menu</h3>
+
+    <div class="menu">
+      <router-link to="/" class="button">
+        <span class="material-icons">home</span>
+        <span class="text">Home</span>
+      </router-link>
+      <router-link to="#" class="button">
+        <span class="material-icons">description</span>
+        <span class="text">About</span>
+      </router-link>
+      <router-link to="#" class="button">
+        <span class="material-icons">group</span>
+        <span class="text">Team</span>
+      </router-link>
+      <router-link to="#" class="button">
+        <span class="material-icons">email</span>
+        <span class="text">Contact</span>
+      </router-link>
+    </div>
+
+    <div class="flex"></div>
+
+    <div class="menu">
+      <router-link to="/" class="button">
+        <span class="material-icons">settings</span>
+        <span class="text">Settings</span>
+      </router-link>
+    </div>
+  </aside>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import logoURL from '../assets/images/logo.png';
+
+const is_expanded = ref(
+  localStorage.getItem('is_expanded') === 'true'
+);
+
+const ToggleMenu = () => {
+  is_expanded.value = !is_expanded.value;
+  localStorage.setItem('is_expanded', is_expanded.value);
+};
+</script>
+
+<style lang="scss">
+@import '../assets/sass/components/sidebar';
+</style>
