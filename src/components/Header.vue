f4d1b2ac596f1d7dd21ee9b7fa0dfb7d2213bd7b
TranNhan <trannhan.s3@gmail.com>
2022-04-10 09:02:14 +0700

    header

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="header">
+    <div class="header__inner container">
+      <div class="header__logo">
+        <img :src="logo" alt="Logo" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { logo } from '../assets/images';
+</script>
+
+<style lang="scss">
+@use '../assets/scss/components/header.scss';
+</style>
