e860976774872bd966035de8236b14e827255f5c
TranNhan <trannhan.s3@gmail.com>
2022-04-10 17:43:52 +0700

    section

---
+++ src/components/Button.vue
@@ -0,0 +1,15 @@
+<template>
+  <button class="btn" :class="props.class">
+    <slot />
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  class: String,
+});
+</script>
+
+<style lang="scss">
+@use '../assets/scss/components/button';
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,14 +4,21 @@
       <div class="header__logo">
         <img :src="logo" alt="Logo" />
       </div>
+      <ul class="header__nav">
+        <li><a href="#">overview</a></li>
+        <li><a href="#">news</a></li>
+        <li><a href="#">champions</a></li>
+        <li><a href="#">support</a></li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup>
 import { logo } from '../assets/images';
+import Button from './Button.vue';
 </script>
 
 <style lang="scss">
-@use '../assets/scss/components/header.scss';
+@use '../assets/scss/components/header';
 </style>
