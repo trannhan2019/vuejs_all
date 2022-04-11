334e9dc3472c6a59ae7d3e64c9cf2f5f623b2b3e
TranNhan <trannhan.s3@gmail.com>
2022-04-11 20:00:53 +0700

    section

---
+++ src/components/section/Section.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="section" :class="props.class">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  class: String,
+});
+</script>
+
+<style lang="scss">
+@use '../../assets/scss/components/section';
+</style>
---
+++ src/components/section/SectionContent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    class="section__content"
+    :class="props.class"
+    :style="props.bgImage"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  class: String,
+  bgImage: Object,
+});
+
+const bgImage = props.bgImage
+  ? { backgroundImage: `url(${props.bgImage})` }
+  : {};
+</script>
+
+<style lang="scss">
+@use '../../assets/scss/components/section';
+</style>
