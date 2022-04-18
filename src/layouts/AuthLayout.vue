98ce7c5b1d425cc7c0fde45743acad2fa91cd66e
TranNhan <trannhan.s3@gmail.com>
2022-04-18 21:12:48 +0700

    register

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,32 @@
+<script></script>
+
+<template>
+  <div
+    class="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8"
+  >
+    <div>
+      <img
+        class="mx-auto h-12 w-auto"
+        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+        alt="Workflow"
+      />
+      <h2
+        class="mt-6 text-center text-3xl font-extrabold text-gray-900"
+      >
+        Sign in to your account
+      </h2>
+      <p class="mt-2 text-center text-sm text-gray-600">
+        Or
+        {{ ' ' }}
+        <a
+          href="#"
+          class="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          start your 14-day free trial
+        </a>
+      </p>
+
+      <slot />
+    </div>
+  </div>
+</template>
