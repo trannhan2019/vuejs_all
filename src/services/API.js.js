becb92d0d293389bcddc223024bf374b1d1dc1a1
TranNhan <trannhan.s3@gmail.com>
2022-04-24 15:17:15 +0700

    24.04.2022

---
+++ src/services/API.js.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+
+export const apiClient = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  withCredentials: true, //required to handle the CSRF token
+});
---
+++ src/services/UserService.js
@@ -0,0 +1,1 @@
+import * as API from '@/services/API.js';
